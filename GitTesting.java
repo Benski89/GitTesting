f187c5f85b0636486fb7449dcb387f0be864dd38
Benski89 <benski@live.co.uk>
2022-03-01 07:37:07 +0000

    GitTesting commit 2

--- GitTesting.java
+++ GitTesting.java
@@ -5,7 +5,8 @@ public class GitTesting {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
-		System.out.println("Hello this is my second change");
+		System.out.println("Hello adding more tests");
+		System.out.println("Hello adding more tests 3");
 
 	}
 
