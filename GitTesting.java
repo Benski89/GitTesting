784333676f5c28ef2e3830a0b85c1cee6014ef22
Benski <benski@benskis-mbp.home>
2022-03-01 00:30:19 +0000

    GitTesting commit 1
     Committer:  Benski <benski@live.co.uk>

---
+++ GitTesting.java
@@ -0,0 +1,13 @@
+package gittraining;
+
+public class GitTesting {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("Hello this is my second change");
+
+	}
+
+}
+ 
